d7af70e62f813b806e2cd8dc1589ae61135c39d6
thealinivan <ivana@roehampton.ac.uk>
2021-05-17 14:02:05 +0100

    AI: resolved task1

--- src/main/java/peer/Runner.java
+++ src/main/java/peer/Runner.java
@@ -3,8 +3,10 @@ package peer;
 public class Runner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Task 1
+		Task1 t1 = new Task1();
+		System.out.println(t1.getFactorial(120));
+		
 	}
 
 }
--- src/test/java/peer/TestTask1.java
+++ src/test/java/peer/TestTask1.java
@@ -1,5 +1,48 @@
 package peer;
 
-public class TestTask1 {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestTask1 {
+ 	Task1 t1 = new Task1();
+ 	
+    @BeforeClass
+    public static void start() {
+        
+    }
+    
+    @Before
+    public void before() {
+  
+    }
+    
+ 
+    @Test
+    public void TestGetFactorial() {
+    	assertEquals("Number is 5!", t1.getFactorial(120));
+    	assertEquals("Number is not factorial", t1.getFactorial(150));
+    }
+    
+    
+    
+
+ 
+
+    
+    @After
+    public void after() {
+
+    }
+    
+    @AfterClass
+    public static void end() {
+
+    }
 }
+
+
